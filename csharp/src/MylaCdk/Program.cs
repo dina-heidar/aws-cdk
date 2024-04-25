6527f63fc1cc27bc42c96eae54143559a89c9b1c
dheida1 <dinaheidar13@gmail.com>
2024-04-25 09:06:41 -0500

    additional comments

--- csharp/src/MylaCdk/Program.cs
+++ csharp/src/MylaCdk/Program.cs
@@ -12,13 +12,14 @@ namespace MyLACdk
 
         public static void Main(string[] args)
         {
+            var app = new App();
+
             // The account and region in which this stack is deployed.
             var env = new Amazon.CDK.Environment
             {
                 Account = "654654599146",
                 Region = "us-east-1"
-            };
-            var app = new App();
+            };          
 
             // A CDK app can contain multiple stacks. You can view a list of all the stacks in your
             // app by typing `cdk list`.
@@ -26,10 +27,12 @@ namespace MyLACdk
             //creates vpc, subnets, clusters, hosted zone
             var netBaseCStack = new NetBaseCStack(app, "NetBaseCStack", new NetBaseCProps
             {
+                //substitute with your personal variable values here
+                //in this section below
                 ClientName = "dina", //agency name?
                 EnvName = EnvName.DEV,
-                Hosted = "ecs.my.la.gov",
-                HostedAnywhere = "ecs-anywhere.my.la.gov",
+                Hosted = "ecs.my.la.gov", //the subdomain name that will be created in Route 53
+                HostedAnywhere = "ecs-anywhere.my.la.gov", //the DNS entry and subdomain created by DS Admin
                 Cidr = "10.13.0.0/16",
                 Env = env
             });
@@ -52,6 +55,7 @@ namespace MyLACdk
                 Cluster = netBaseCStack.cluster,
                 Rds = stateFulCStack.rds,
                 Hosted = netBaseCStack.hosted,
+                //previously created certificate in AWS ACM
                 CertificateArn = "arn:aws:acm:us-east-1:654654599146:certificate/72fcdfb5-addf-4846-8883-07c41e6edf40",
                 Region = netBaseCStack.Region,
                 Zone = netBaseCStack.zone,
--- typescript/bin/myla-cdk.ts
+++ typescript/bin/myla-cdk.ts
@@ -16,11 +16,13 @@ const env = {account: '654654599146',  region: 'us-east-1' }
 const app = new cdk.App();
 
 //creates vpc, subnets, clusters, hosted zone
-const netBaseStack = new NetBaseStack(app, 'NetBaseStack', {  
+const netBaseStack = new NetBaseStack(app, 'NetBaseStack', { 
+  //substitute with your personal variable values here
+  //in this section below
   clientName: 'dina', //agency name?
   envName: EnvName.DEV,
-  hosted: "ecs.my.la.gov",
-  hostedAnywhere: "ecs-anywhere.my.la.gov",
+  hosted: "ecs.my.la.gov", //the subdomain name that will be created in Route 53
+  hostedAnywhere: "ecs-anywhere.my.la.gov", //the DNS entry and subdomain created by DS Admin
   cidr: "10.13.0.0/16",
   env
 });
@@ -41,6 +43,7 @@ new EcsStack(app, 'EcsStack', {
     cluster: netBaseStack.cluster,   
     rds: statefulStack.rds,
     hosted: netBaseStack.hosted,
+     //previously created certificate in AWS ACM
     certificateArn: 'arn:aws:acm:us-east-1:654654599146:certificate/72fcdfb5-addf-4846-8883-07c41e6edf40',
     region: netBaseStack.region,
     zone: netBaseStack.zone,
